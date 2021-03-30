41a7e9cd44c31931d6c83579714bafc77eaacd68
Corey Koontz <corey.michael.koontz@gmail.com>
2021-03-30 10:46:17 -0500

    adding insert statments to warmup

--- transactions_migration.sql
+++ transactions_migration.sql
@@ -9,5 +9,8 @@ CREATE TABLE transactions (
     memo TEXT,
     amount DECIMAL (8,2) NOT NULL,
     transaction_successful TINYINT
-
 );
+
+INSERT INTO transactions (date, amount, description)
+VALUES (2021-05-30, 1000.00, 'food')
+(2021-04-13, 500.45, 'cake');
