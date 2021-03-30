491196de0d15cb0b4fb43acfb521995cd3dfc6df
Corey Koontz <corey.michael.koontz@gmail.com>
2021-03-30 15:03:02 -0500

    adding second selects to update_exercise

--- update_exercise.sql
+++ update_exercise.sql
@@ -8,6 +8,9 @@ use codeup_test_db;
 -- UPDATE albums
 -- SET sales = sales * 10;
 
+-- SELECT *
+-- FROM albums;
+
 -- ----------------------------------------------------------
 -- SELECT 'before 1980' AS 'All albums from:';
 -- SELECT *
@@ -16,6 +19,10 @@ use codeup_test_db;
 --
 -- UPDATE albums
 -- SET release_date = release_date - 100
+-- WHERE release_date < 1980;
+
+-- SELECT *
+-- FROM albums
 -- WHERE release_date < 1980;
 
 -- ----------------------------------------------------------
@@ -27,4 +34,10 @@ use codeup_test_db;
 -- UPDATE albums
 -- SET artist = 'Peter Jackson'
 -- WHERE artist = 'Michael Jackson';
+
+-- SELECT *
+-- FROM albums
+-- WHERE artist = 'Michael Jackson';
+
+
 
