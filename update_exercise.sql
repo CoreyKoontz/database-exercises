4131eb55502e46df1bf6bdad206ca55da3e75438
Corey Koontz <corey.michael.koontz@gmail.com>
2021-03-30 14:55:08 -0500

    Adding update_exercise

---
+++ update_exercise.sql
@@ -0,0 +1,30 @@
+use codeup_test_db;
+
+-- ----------------------------------------------------------
+-- SELECT 'table' AS 'All albums from:';
+-- SELECT *
+-- FROM albums;
+--
+-- UPDATE albums
+-- SET sales = sales * 10;
+
+-- ----------------------------------------------------------
+-- SELECT 'before 1980' AS 'All albums from:';
+-- SELECT *
+-- FROM albums
+-- WHERE release_date < 1980;
+--
+-- UPDATE albums
+-- SET release_date = release_date - 100
+-- WHERE release_date < 1980;
+
+-- ----------------------------------------------------------
+-- SELECT 'Michael Jackson' AS 'All albums by:';
+-- SELECT *
+-- FROM albums
+-- WHERE artist = 'Michael Jackson';
+--
+-- UPDATE albums
+-- SET artist = 'Peter Jackson'
+-- WHERE artist = 'Michael Jackson';
+
