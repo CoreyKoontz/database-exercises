6f516db2469dfdf4037402ac52641f93b1484081
Corey Koontz <corey.michael.koontz@gmail.com>
2021-03-31 17:59:48 -0500

    Adding order-by exercise

---
+++ order_by_exercises.sql
@@ -0,0 +1,51 @@
+use employees;
+
+-- ---------------- Part One --------------------------------
+
+SELECT *
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+ORDER BY last_name, first_name;
+
+SELECT *
+FROM employees
+WHERE last_name LIKE 'E%'
+ORDER BY last_name;
+
+SELECT *
+FROM employees
+WHERE last_name LIKE '%q%';
+
+-- ---------------- Part Two --------------------------------
+
+SELECT *
+FROM employees
+WHERE first_name = 'Irena'
+   OR first_name = 'Vidya'
+   OR first_name = 'Maya';
+
+SELECT *
+FROM employees
+WHERE gender = 'M'
+  AND (
+            first_name = 'Irena'
+        OR first_name = 'Vidya'
+        OR first_name = 'Maya'
+    );
+
+SELECT *
+FROM employees
+WHERE last_name LIKE 'E%'
+   OR last_name LIKE '%E'
+ORDER BY last_name;
+
+SELECT *
+FROM employees
+WHERE last_name LIKE 'E%'
+  AND last_name LIKE '%E'
+ORDER BY last_name;
+
+SELECT *
+FROM employees
+WHERE last_name LIKE '%q%'
+  AND last_name NOT LIKE '%qu%';
