08aa0a5ff53f2b0da0ad94bec2febfc503488759
Corey Koontz <corey.michael.koontz@gmail.com>
2021-04-07 19:35:17 -0500

    Adding new table(user_info) to DB

--- adlistener_db.sql
+++ adlistener_db.sql
@@ -1,3 +1,5 @@
+DROP DATABASE ad_listener_db;
+
 CREATE
     DATABASE IF NOT EXISTS ad_listener_db;
 
@@ -8,20 +10,34 @@ DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS ads;
 DROP TABLE IF EXISTS categories;
 
--- ---------------------------------------------------------- USERS TABLE:
-CREATE TABLE users
+-- ---------------------------------------------------------- USERS_CREDENTIALS TABLE:
+CREATE TABLE users_credentials
 (
     id       INT UNSIGNED NOT NULL AUTO_INCREMENT,
     email    VARCHAR(100) NOT NULL,
     password VARCHAR(100) NOT NULL, -- Probably need to change type
     PRIMARY KEY (id)
 );
-INSERT INTO users (email, password)
+INSERT INTO users_credentials (email, password)
 VALUES ('tom@user.com', 'tompass'),
        ('jill@user.com', 'jillpass'),
        ('mike@user.com', 'mikepass'),
        ('kim@user.com', 'kimpass');
 
+
+-- ---------------------------------------------------------- USER_INFO TABLE:
+CREATE TABLE user_info
+(
+    user_id        INT UNSIGNED NOT NULL PRIMARY KEY,
+    first_name     VARCHAR(100)NOT NULL,
+    last_name      VARCHAR(100)NOT NULL,
+    phone_no       VARCHAR(30)  DEFAULT 'N/A',
+    street_address VARCHAR(100) DEFAULT 'N/A',
+    city           VARCHAR(100) DEFAULT 'N/A',
+    state          VARCHAR(3)   DEFAULT 'N/A',
+    joined_on      DATETIME     DEFAULT NOW(),
+    FOREIGN KEY (user_id) REFERENCES users_credentials (id)
+);
 -- ---------------------------------------------------------- ADS TABLE:
 CREATE TABLE ads
 (
@@ -31,7 +47,7 @@ CREATE TABLE ads
     price        DECIMAL(6, 2),
     created_date DATETIME DEFAULT NOW(),
     user_id      INT UNSIGNED NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users (id),
+    FOREIGN KEY (user_id) REFERENCES users_credentials (id),
     PRIMARY KEY (id)
 );
 
@@ -62,7 +78,7 @@ VALUES ('Automobiles'),
 -- ---------------------------------------------------------- AD_CATEGORIES TABLE
 CREATE TABLE ad_categories
 (
-    ad_id     INTEGER UNSIGNED NOT NULL,
+    ad_id       INTEGER UNSIGNED NOT NULL,
     category_id INTEGER UNSIGNED NOT NULL,
     FOREIGN KEY (ad_id) REFERENCES ads (id),
     FOREIGN KEY (category_id) REFERENCES categories (id)
@@ -83,15 +99,15 @@ VALUES (1, 1),
 
 SELECT u.email AS User, a.title AS 'Item'
 FROM users AS u
-JOIN ads a
-    on u.id = a.user_id
+         JOIN ads a
+              on u.id = a.user_id
 WHERE a.title = 'honda car';
 
 # TODO: For a given ad, what category, or categories, does it belong to?
 SELECT a.title AS 'Item', c.name AS 'Categories'
 FROM ads a
-JOIN categories c
-    on a.id = c.id
+         JOIN categories c
+              on a.id = c.id
 WHERE a.title = 'iphone';
 
 
